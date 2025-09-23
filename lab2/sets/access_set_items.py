51b8e33df52b315b7eec0d09422fec470e0af2c2
Didar <kalabaevdidar942@gmail.com>
2025-09-23 18:42:13 +0500

    lab2

---
+++ lab2/sets/access_set_items.py
@@ -0,0 +1,16 @@
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+
+
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" not in thisset)
